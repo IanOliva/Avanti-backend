5a3f21c75877c527caad5beff8c044a8083e5f48
Ian <ianoliva2000@gmail.com>
2025-02-01 17:33:52 -0300

    intento de correcion sawgger

--- mi-api/config/swagger.js
+++ mi-api/config/swagger.js
@@ -2,7 +2,7 @@ import swaggerUi from "swagger-ui-express";
 
 import fs from "fs";
 
-const swaggerDocument = JSON.parse(fs.readFileSync("../swagger_output.json", "utf-8"));
+const swaggerDocument = JSON.parse(fs.readFileSync("./swagger_output.json", "utf-8"));
 
 const setupSwagger = (app) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
